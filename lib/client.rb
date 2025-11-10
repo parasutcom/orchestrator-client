8db1d1821373bbe2b19a5d18ff49897c4370b535
berksurmeli <hasan.surmeli@parasut.com>
2025-11-10 09:39:05 +0300

    Refactor client configuration to support multiple named clients

--- lib/client.rb
+++ lib/client.rb
@@ -1,24 +1,40 @@
 # frozen_string_literal: true
 
-require_relative 'client/version'
-require_relative 'client/config'
-require_relative 'client/errors'
-require_relative 'client/hmac'
-require_relative 'client/http'
+require_relative "client/version"
+require_relative "client/config"
+require_relative "client/errors"
+require_relative "client/hmac"
+require_relative "client/http"
 
 module Client
+  @clients = {} # registry of named client configs
+
   class << self
+    # For backward compatibility (single-client mode)
     def config
-      @config ||= Client::Config.new
+      @clients[:default] ||= Client::Config.new
     end
 
-    def configure
-      yield(config)
+    # Configure a specific client (or default)
+    def configure(name = :default)
+      @clients[name.to_sym] ||= Client::Config.new
+      yield(@clients[name.to_sym])
     end
 
-    # POST /executions
-    def execute(operation:, caller_id:, params: {}, async: false, idem_key: nil, timeouts: nil)
-      Client::HTTP.new(config: config).execute(
+    # Retrieve configuration for a named client
+    def [](name = :default)
+      @clients[name.to_sym] || raise("No client configured for #{name.inspect}")
+    end
+
+    # Return all configured clients
+    def all
+      @clients
+    end
+
+    # --- Client API (bound to a specific config) ---
+
+    def execute(client_name = :default, operation:, caller_id:, params: {}, async: false, idem_key: nil, timeouts: nil)
+      http(client_name).execute(
         operation: operation,
         caller_id: caller_id,
         params: params,
@@ -28,27 +44,23 @@ module Client
       )
     end
 
-    # GET /executions/fetch_tasks
-    def fetch_tasks(caller_id:)
-      Client::HTTP.new(config: config).fetch_tasks(
-        caller_id: caller_id
-      )
+    def fetch_tasks(client_name = :default, caller_id:)
+      http(client_name).fetch_tasks(caller_id: caller_id)
     end
 
-    # GET /executions/fetch_task?task=<task_name>
-    def fetch_task(task_name:, caller_id:)
-      Client::HTTP.new(config: config).fetch_task(
-        task_name: task_name,
-        caller_id: caller_id
-      )
+    def fetch_task(client_name = :default, task_name:, caller_id:)
+      http(client_name).fetch_task(task_name: task_name, caller_id: caller_id)
     end
 
-    # GET /executions/wait/:idem_key
-    def wait(idem_key:, caller_id:)
-      Client::HTTP.new(config: config).wait(
-        idem_key: idem_key,
-        caller_id: caller_id
-      )
+    def wait(client_name = :default, idem_key:, caller_id:)
+      http(client_name).wait(idem_key: idem_key, caller_id: caller_id)
+    end
+
+    private
+
+    def http(client_name)
+      cfg = @clients[client_name.to_sym] || raise("Client not configured: #{client_name.inspect}")
+      Client::HTTP.new(config: cfg)
     end
   end
 end
